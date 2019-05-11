cec2ce4f352418d5600b7fb89b4e86533bfb09c8
Meng Li <lm2343635@126.com>
2019-05-11 21:35:03 +0900

    Create mac-init-shell.sh

---
+++ mac-init-shell.sh
@@ -0,0 +1,8 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install wget
+brew cask install balenaetcher
+brew cask install iina
+brew cask install docker
+sudo gem install fastlane -NV
+sudo gem install cocoapods
+sudo gem install cocoa pods-binary
