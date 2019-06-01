553a7d9f8659a40164953371f8c02b05f6fd2a5e
Meng Li <lm2343635@126.com>
2019-06-01 23:53:55 +0900

    Update mac-init-shell.sh

--- mac-init-shell.sh
+++ mac-init-shell.sh
@@ -1,6 +1,9 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo gem install fastlane -NV
+sudo gem install cocoapods
+sudo gem install cocoa pods-binary
 brew cask install google-chrome
-brew cask install sogouinput
 brew cask install visual-studio-code
 brew cask install sublime-text
 brew cask install atom
@@ -12,6 +15,3 @@ brew cask install docker
 brew install wget
 brew install node
 brew install maven
-sudo gem install fastlane -NV
-sudo gem install cocoapods
-sudo gem install cocoa pods-binary
