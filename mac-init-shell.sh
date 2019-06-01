54d4ea09357726b72a37f616658f7c08c0ec77f4
Meng Li <lm2343635@126.com>
2019-06-01 23:10:59 +0900

    Update mac-init-shell.sh

--- mac-init-shell.sh
+++ mac-init-shell.sh
@@ -1,12 +1,17 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew cask install google-chrome
+brew cask install sogouinput
+brew cask install visual-studio-code
+brew cask install sublime-text
+brew cask install atom
+brew cask install balenaetcher
+brew cask install iina
+brew cask install sourcetree
+brew cask install macdown
+brew cask install docker
 brew install wget
 brew install node
 brew install maven
-brew cask install sogouinput
-brew cask install visual-studio-code
-brew cask install balenaetcher
-brew cask install iina
-brew cask install docker
 sudo gem install fastlane -NV
 sudo gem install cocoapods
 sudo gem install cocoa pods-binary
