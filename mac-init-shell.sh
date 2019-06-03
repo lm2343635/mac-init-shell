63d7bc1e9c5667a5186c263e32f2b59b57086423
Meng Li <lm2343635@126.com>
2019-06-03 21:33:46 +0900

    Update mac-init-shell.sh

--- mac-init-shell.sh
+++ mac-init-shell.sh
@@ -1,3 +1,4 @@
+sudo spctl --master-disable
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sudo gem install fastlane -NV
